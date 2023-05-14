0d71ff055cc8b75e33acc5a5a6953246257b7a2b
Ahmed Aluthman <amo.1980@gmail.com>
2023-05-14 19:12:38 +0300

    v0.22 changed my mind. will implement api myself

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+extern crate url;
+extern crate reqwest;
+
+pub mod apis;
+pub mod models;
+
 use serde::Deserialize;
 use serde_json::Value;
 use std::collections::HashMap;
 use std::fs;
+
+//use lib::apis::
+
+use apis::configuration::Configuration;
+use apis::agents_api;
 
 use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_LENGTH, CONTENT_TYPE, HOST};
 const ST_API_URL: &str = "https://api.spacetraders.io/v2";
@@ -10,6 +26,11 @@ const ST_API_AGENT: &str = "/my/agent";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let token = fs::read_to_string("token.txt")?;
+    // let mut conf = Configuration::new();
+    // conf.bearer_access_token = Some(token);
+    // let myagent = agents_api::get_my_agent(&conf);
+    // println!("{:#?}",myagent.await?);
+
 
     //let client = reqwest::Client::new();
     //let mut headers = HeaderMap::new();
