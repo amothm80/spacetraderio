99c2616a15ebbd04ebcad362de32eb5c35bd496a
hammoudith <amo.1980@gmail.com>
2023-06-05 20:44:32 +0300

    v0.271

--- src/models/waypointtrait.rs
+++ src/models/waypointtrait.rs
@@ -1,7 +1,6 @@
 use serde_derive::Deserialize;
 use serde_derive::Serialize;
 
-
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
